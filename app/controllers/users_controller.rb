42a347f0e7b7cc97bb1733388b29211ba1565238
RFoltynski <Foltynski.rafal@gmail.com>
2019-09-12 22:52:38 +0200

    added: active storage, users controller, avatar to user, users resources and show user view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  before_action :user_params, only: %i[show]
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private   
+  def user_params 
+    params.permit(:email, :id, :avatr)
+  end
+end
