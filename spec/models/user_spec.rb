ce87b78786a5820e0317b91070515452169b1419
RFoltynski <Foltynski.rafal@gmail.com>
2019-09-13 07:25:12 +0200

    validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validation" do
+    it { is_expected.to validate_presence_of(:nick) }
+    it { is_expected.to validate_uniqueness_of(:nick) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
 end
+  
