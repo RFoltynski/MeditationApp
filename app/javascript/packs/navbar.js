bfc46cd8f45440667363deea3251303b68a1c1dd
RFoltynski <Foltynski.rafal@gmail.com>
2019-10-09 21:37:39 +0200

    navbar layout added

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 
 class Navbar extends Component {
   render() {
-    return <div className="navbar">Navbar</div>;
+    return (
+      <div className="navbar">
+        <div className="logo">Medi</div> <a href="/users/edit">Edytuj Profil</a>{" "}
+        <a href="/users/sign_out">Wyloguj</a>
+      </div>
+    );
   }
 }
 export default ReactDOM.render(<Navbar />, document.getElementById("navbar"));
