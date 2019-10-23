28576632835b46038eacb9471ee75713a7c77f3c
foltynski.rafal <foltynski.rafal@gmail.com>
2019-10-23 14:51:27 +0200

    improvments in meditations controller
    small code refactoring
    fix edit meditation bug

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -1,21 +1,19 @@
 class MeditationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_meditation, only: %i[show destroy edit]
+  before_action :find_meditation, only: %i[show destroy edit update]
   before_action :meditation_params, only: %i[create update]
+  before_action :set_collections, only: %i[new create edit update]
   def index
     @meditations = Meditation.all
   end
 
   def new
-    @collections = Collection.all.map{ |c| [c.name, c.id] }
     @meditation = Meditation.new
-    
   end
 
   def create
     @meditation = Meditation.new(meditation_params)
     @meditation.collection_id = params[:collection_id]
-    @collections = Collection.all.map{|c| [ c.name, c.id ] }
     if @meditation.save 
       flash[:notice] = "Medytacja została dodana."
       redirect_to meditations_path
@@ -26,15 +24,13 @@ class MeditationsController < ApplicationController
 
   def show; end
 
-  def edit
-    @collections = Collection.all.map{|c| [c.name, c.id]}
-  end
+  def edit; end
 
   def update
     @meditation.collection_id = params[:collection_id]
-
     if @meditation.update(meditation_params)
-      redirect_to meditation_path(@meditation)
+      flash[:notice] = "Medytacja została edytowana."
+      redirect_to meditations_path
     else
       render 'edit'
     end
@@ -53,5 +49,9 @@ class MeditationsController < ApplicationController
 
   def find_meditation
     @meditation = Meditation.find(params[:id])
+  end
+
+  def set_collections 
+    @collections = Collection.all.map{ |c| [c.name, c.id] }
   end
 end
