2c24459e7e2971a1135463fc8fe1450ff6cd010e
foltynski.rafal <foltynski.rafal@gmail.com>
2019-10-24 06:40:32 +0200

    play meditation basic settings
    add the js pack tag in show view of meditations
    add play-page.jsx file
    install axios

---
+++ app/javascript/packs/components/play-page.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class MeditationPlay extends Component {
+  render() {
+    return (
+      <div>Play meditation</div>
+    );
+  }
+}
+export default ReactDOM.render(<MeditationPlay />, document.getElementById("play-page"));
