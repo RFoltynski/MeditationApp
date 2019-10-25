371dd1499f72e0f9b9ed19b868685ad8f807ea35
foltynski.rafal <foltynski.rafal@gmail.com>
2019-10-25 12:03:54 +0200

    add basic meditation player

--- app/javascript/packs/components/play-page.jsx
+++ app/javascript/packs/components/play-page.jsx
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
+import ReactAudioPlayer from "react-audio-player";
 
 class MeditationPlay extends Component {
-  // componentWillMount() {
-  //   axios
-  //     .get("api/v1/intentions.json", {}, { "Content-Type": "application/json" })
-  //     .then(res => {
-  //       this.setState({
-  //         intentions: res.data.data,
-  //         isLoading: true
-  //       });
-  //     });
-  // }
+  state = {
+    isLoading: false
+  };
+
+  componentWillMount() {
+    const currentHref = window.location.href.split("/");
+    const id = currentHref[currentHref.length - 1];
+
+    axios
+      .get(
+        `http://localhost:3000/api/v1/meditations/${id}`,
+        {},
+        { "Content-Type": "application/json" }
+      )
+      .then(res => {
+        this.setState({
+          intentions: res.data.data,
+          isLoading: true
+        });
+      });
+  }
 
   render() {
-    return <div>Play meditation</div>;
+    let audio = this.state.isLoading
+      ? this.state.intentions.meditationFile
+      : "";
+    return (
+      <div>
+        <div>
+          <ReactAudioPlayer src={audio} controls />
+        </div>
+      </div>
+    );
   }
 }
 export default ReactDOM.render(
