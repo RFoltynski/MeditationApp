2a02d46332a98dcc488466ef1a70dc47710838f3
foltynski.rafal <foltynski.rafal@gmail.com>
2019-11-01 22:05:32 +0100

    comments
    add creat function in controller
    add comments partial
    add comments form partial
    add render in meditation view file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,22 @@
 class CommentsController < ApplicationController
   def index
   end
+
+  def create
+    @meditation = Meditation.find(params[:meditation_id])
+    @comment = @meditation.comments.create(comment_params)
+
+    @comment.user_id = current_user.id
+
+    if @comment.save 
+      redirect_to meditation_path(@meditation)
+    else 
+      render 'new'
+    end
+  end
+
+  private 
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
