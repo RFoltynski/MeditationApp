dc513f1990a3f13aa476d159a0bf3165747da8d4
foltynski.rafal <foltynski.rafal@gmail.com>
2019-11-02 07:15:05 +0100

    add edit and destroy actions to comments
    add update, destroy and edit methodts to comments controller
    add links in in comments view
    add edit file comments view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,8 @@
 class CommentsController < ApplicationController
-  def index
-  end
-
+  before_action :find_meditation, only: [:create, :edit, :update, :destroy]
+  before_action :find_comment, only: [:edit, :update, :destroy]
   def create
-    @meditation = Meditation.find(params[:meditation_id])
     @comment = @meditation.comments.create(comment_params)
-
     @comment.user_id = current_user.id
 
     if @comment.save 
@@ -15,8 +12,29 @@ class CommentsController < ApplicationController
     end
   end
 
-  private 
+  def update
+    if @comment.update(comment_params)
+      redirect_to meditation_path(@meditation)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to meditation_path(@meditation)
+  end
+
+  private
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def find_meditation
+    @meditation = Meditation.find(params[:meditation_id])
+  end
+
+  def find_comment
+    @comment = @meditation.comments.find(params[:id])
   end
 end
