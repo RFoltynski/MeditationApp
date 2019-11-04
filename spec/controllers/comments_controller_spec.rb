ce1296fcd20a422133ec6b32dfd95c979ad0f15b
foltynski.rafal <foltynski.rafal@gmail.com>
2019-11-04 18:16:53 +0100

    add comments model tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,29 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  let(:user) { create(:user) }
 
-  before { sign_in(user) }
-
-  describe 'POST #create' do 
-
-
-  let(:valid_collection) { { comment: { content: 'testsdfasdf', meditation_id: 38, user_id: user.id } } }
-  let(:invalid_collection) { { comment: { content: '', meditation_id: 38, user_id: user.id  } } }
-
-  context 'valid attributes' do
-    
-    subject { post :create, params: valid_collection }
-
-    it { expect(subject).to redirect_to("/meditations/38") }
-    it { expect { subject }.to change(Comment, :count).by(1) }
-  end
-  
-  context 'invalid attributes' do
-    subject { post :create, params: invalid_collection }
-
-    it { expect { subject }.to change(Comment, :count).by(0) }
-  end
-
-end
 end
